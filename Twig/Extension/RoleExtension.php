1d2d7efa44575a32c79ea559975fa5dea36abd0f
João Paulo Cercal <joao.cercal@a2c.com.br>
2013-12-04 19:11:15 -0200

    Corrigido incompatibilidades com o annotation @Template(), pois, ele não está disponível via xml e yaml. Por conta disso, obtemos o controller a partir de expressões regulares.

--- Twig/Extension/RoleExtension.php
+++ Twig/Extension/RoleExtension.php
@@ -15,9 +15,9 @@ class RoleExtension extends \Twig_Extension
 
     const ROLE_PREFIX           = 'ROLE';
     const ROLE_SEPARATOR        = '_';
-    
+
     /**
-     * @var \Symfony\Component\DependencyInjection\ContainerInterface 
+     * @var \Symfony\Component\DependencyInjection\ContainerInterface
      */
     protected $container;
 
@@ -28,71 +28,85 @@ class RoleExtension extends \Twig_Extension
     {
         $this->container = $container;
     }
-    
+
     /**
      * Recupera uma instância de ContainerInterface
-     * 
+     *
      * @return \Symfony\Component\DependencyInjection\ContainerInterface
      */
     protected function getContainer()
     {
         return $this->container;
     }
-    
+
     /**
      * Atalho para recuperar uma instância de SecurityContext
-     * 
+     *
      * @return \Symfony\Component\Security\Core\SecurityContext
      */
     protected function getSecurity()
     {
         return $this->getContainer()->get('security.context');
     }
-    
+
     /**
      * Atalho para recuperar uma instância de Request
-     * 
+     *
      * @return \Symfony\Component\HttpFoundation\Request
      */
-    protected function getRequest() 
+    protected function getRequest()
     {
         return $this->getContainer()->get('request');
     }
-    
-    /**
-     * Atalho para recuperar uma instância de TemplateReference
-     * 
-     * @return \Symfony\Bundle\FrameworkBundle\Templating\TemplateReference
-     */
-    protected function getTemplate() 
-    {
-        return $this->getRequest()->attributes->get('_template');
-    }
-
 
     /**
      * Recupera o nome do bundle
-     * 
+     *
      * @return string
      */
     protected function getBundleName()
     {
-        return strtoupper(substr($this->getTemplate()->get('bundle'), 0, -6));
+        $matches    = array();
+        $controller = 'FOS\UserBundle\Controller\ProfileController::showAction';
+
+        preg_match('/(.*)Bundle/', $controller, $matches);
+
+        return $matches[1];
     }
-    
+
     /**
      * Recupera o nome do controller
-     * 
+     *
      * @return string
      */
     protected function getControllerName()
     {
-        return strtoupper($this->getTemplate()->get('controller'));
+        $pattern = "#Controller\\\([a-zA-Z]*)Controller#";
+        $matches = array();
+
+        preg_match($pattern, $this->getRequest()->get('_controller'), $matches);
+
+        return $matches[1];
     }
-    
+
+    /**
+     * Recupera o nome da action
+     *
+     * @return string
+     */
+    protected function getActionName()
+    {
+        $pattern = "#::([a-zA-Z]*)Action#";
+        $matches = array();
+
+        preg_match($pattern, $this->getRequest()->get('_controller'), $matches);
+
+        return $matches[1];
+    }
+
     /**
      * Recupera o nome do papel utilizado pelo administrador
-     * 
+     *
      * @return string
      */
     protected function getRoleNameAdmin()
@@ -120,20 +134,21 @@ class RoleExtension extends \Twig_Extension
     public function isGranted($role)
     {
         $role = (empty($role) or 'LIST' === strtoupper($role)) ? '' : self::ROLE_SEPARATOR . strtoupper($role);
-        
-        $role = self::ROLE_PREFIX 
-            . self::ROLE_SEPARATOR 
-            . $this->getBundleName()
-            . self::ROLE_SEPARATOR 
-            . $this->getControllerName()
-            . $role;
-        
+
+        $role = self::ROLE_PREFIX
+            . self::ROLE_SEPARATOR
+            . str_replace('\\', '', strtoupper($this->getBundleName()))
+            . self::ROLE_SEPARATOR
+            . strtoupper($this->getControllerName())
+            . $role
+        ;
+
         return ($this->getSecurity()->isGranted($role) or $this->getSecurity()->isGranted($this->getRoleNameAdmin()));
     }
 
     /**
      * Recupera o nome da extensão do twig
-     * 
+     *
      * @return string
      */
     public function getName()
