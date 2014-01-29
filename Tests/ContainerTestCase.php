56c08fad13af21564a9a4f399f5ec556dec422f3
João Paulo Cercal <joao.cercal@a2c.com.br>
2014-01-29 11:48:05 -0200

    Classes base para a realização de testes unitários com PHPUnit.

---
+++ Tests/ContainerTestCase.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Cekurte\GeneratorBundle\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Doctrine\ORM\EntityManager;
+
+/**
+ * @author João Paulo Cercal <sistemas@cekurte.com>
+ * @version 1.0
+ */
+abstract class ContainerTestCase extends WebTestCase
+{
+    /**
+     * {@inherited}
+     */
+    public function setUp()
+    {
+        static::$kernel = static::createKernel();
+
+        static::$kernel->boot();
+    }
+
+    /**
+     * @var ContainerInterface
+     */
+    public function getContainer()
+    {
+        return static::$kernel->getContainer();
+    }
+
+    /**
+     * @var EntityManager
+     */
+    public function getEntityManager()
+    {
+        return $this->getContainer()->get('doctrine.orm.entity_manager');
+    }
+}
---
+++ Tests/UserTestCase.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace Cekurte\GeneratorBundle\Tests;
+
+use FOS\UserBundle\Model\User;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
+
+/**
+ * @author João Paulo Cercal <sistemas@cekurte.com>
+ * @version 1.0
+ */
+abstract class UserTestCase extends ContainerTestCase
+{
+    protected $user;
+
+    /**
+     * @return \FOS\UserBundle\Doctrine\UserManager
+     */
+    public function getUserManager()
+    {
+        return $this->getContainer()->get('fos_user.user_manager');
+    }
+
+    /**
+     * Recupera ou cria um usuário na base de dados
+     *
+     * @param  string $role
+     * @param  array  $fields os campos adicionais da entidade, exemplo: array('setNome' => 'João Paulo Cercal')
+     *
+     * @return User
+     */
+    public function getUser($role = null, array $fields)
+    {
+        $this->user = $this->getUserManager()->findUserByUsername('test');
+
+        if (!isset($this->user)) {
+
+            $user = $this->getUserManager()->createUser();
+
+            $user->setEnabled(true);
+            $user->setUsername('test');
+            $user->setEmail('test@fakemail.com');
+            $user->setPlainPassword('test');
+
+            foreach ($fields as $method => $value) {
+                $user->{$method}($value);
+            }
+
+            $this->getUserManager()->updatePassword($user);
+
+            if (isset($role)) {
+                $user->addRole($role);
+            }
+
+            $this->getUserManager()->updateUser($user);
+
+            $this->user = $user;
+        }
+
+        return $this->user;
+    }
+
+    /**
+     * @param  User $user
+     *
+     * @return User|null
+     *
+     * @throws \LogicException
+     */
+    public function login(User $user)
+    {
+        $client     = $this->createClient();
+
+        $token      = new UsernamePasswordToken($user, 'password', 'main', $user->getRoles());
+
+        $session    = $this->getContainer()->get('session');
+
+        $client->getContainer()->get('security.context')->setToken($token);
+
+        $session->set('_security_main', serialize($token));
+
+        if (!$this->getContainer()->has('security.context')) {
+            throw new \LogicException('O SecurityBundle não está registrado para esta aplicação.');
+        }
+
+        if (null === $token = $this->getContainer()->get('security.context')->getToken()) {
+            return null;
+        }
+
+        if (!is_object($user = $token->getUser())) {
+            return null;
+        }
+
+        return $user;
+    }
+}
