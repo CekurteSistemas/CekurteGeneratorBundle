c300c69cacf1e0b0a00f889dec8828f883adb4a0
João Paulo Cercal <joao.cercal@a2c.com.br>
2014-04-01 16:52:29 -0300

    A classe CekurteFormHandler sofreu refatoração e padronização com o nome dos métodos, como por exemplo o antigo "getEm()" agora passou a ser chamado de "getManager()" de acordo com os padrões definidos pelo "Doctrine" e também pelo bundle "CekurteComponentBundle".

--- Form/Handler/CekurteFormHandler.php
+++ Form/Handler/CekurteFormHandler.php
@@ -37,15 +37,15 @@ abstract class CekurteFormHandler
 
     /**
      * @param FormInterface $form
-     * @param Request $request
+     * @param Request       $request
      * @param EntityManager $em
-     * @param FlashBag $flashBag
+     * @param FlashBag      $flashBag
      */
     public function __construct(FormInterface $form, Request $request, EntityManager $em, FlashBag $flashBag)
     {
-        $this->form = $form;
-        $this->request = $request;
-        $this->em = $em;
+        $this->form     = $form;
+        $this->request  = $request;
+        $this->em       = $em;
         $this->flashBag = $flashBag;
     }
 
@@ -68,7 +68,7 @@ abstract class CekurteFormHandler
     /**
      * @return EntityManager
      */
-    public function getEm()
+    public function getManager()
     {
         return $this->em;
     }
@@ -82,39 +82,50 @@ abstract class CekurteFormHandler
     }
 
     /**
+     * Verifica se o formulário submetido é válido
+     *
+     * @return boolean
+     */
+    protected function formIsValid()
+    {
+        $this->getForm()->handleRequest($this->getRequest());
+
+        if ($this->getForm()->isValid()) {
+            return true;
+        }
+
+        $errors = $this->getForm()->getErrors();
+
+        foreach ($errors as $error) {
+            $this->getFlashBag()->add('message', array(
+                'type'      => 'error',
+                'message'   => $error->getMessage(),
+            ));
+        }
+
+        return false;
+    }
+
+    /**
      * Cria ou atualiza um registro no banco de dados.
      *
      * @return int|boolean O ID do registro afetado no banco de dados, do contrário, false.
      */
     public function save()
     {
-
-        $this->getForm()->bind($this->getRequest());
-
-        if ($this->getForm()->isValid()) {
+        if ($this->formIsValid()) {
 
             $data = $this->getForm()->getData();
 
             $this->getFlashBag()->add('message', array(
-                'type' => 'success',
-                'message' => sprintf('The record has been %s successfully.', $data->getId() ? 'updated ' : 'created'),
+                'type'      => 'success',
+                'message'   => sprintf('The record has been %s successfully.', $data->getId() ? 'updated ' : 'created'),
             ));
 
-            $this->getEm()->persist($data);
-            $this->getEm()->flush();
+            $this->getManager()->persist($data);
+            $this->getManager()->flush();
 
             return $data->getId();
-        } else {
-            $errors = $this->getForm()->getErrors();
-
-            foreach ($errors as $error) {
-                $this->getFlashBag()->add('message', array(
-                    'type' => 'error',
-                    'message' => $error->getMessage(),
-                ));
-            }
-
-            return false;
         }
 
         return false;
@@ -129,44 +140,33 @@ abstract class CekurteFormHandler
      */
     public function delete($entityName)
     {
+        if ($this->formIsValid()) {
 
-        $this->getForm()->bind($this->getRequest());
-
-        if ($this->getForm()->isValid()) {
-
-            $entity = $this->getEm()->getRepository($entityName)->find($this->getRequest()->request->get('id'));
+            $entity = $this->getManager()->getRepository($entityName)->find(
+                $this->getRequest()->request->get('id')
+            );
 
             if (!$entity) {
 
                 $this->getFlashBag()->add('message', array(
-                    'type' => 'error',
-                    'message' => 'The record was not found.',
+                    'type'      => 'error',
+                    'message'   => 'The record was not found.',
                 ));
 
                 return false;
             }
 
-            $this->getEm()->remove($entity);
-            $this->getEm()->flush();
+            $this->getManager()->remove($entity);
+            $this->getManager()->flush();
 
             $this->getFlashBag()->add('message', array(
-                'type' => 'success',
-                'message' => 'The record has been removed successfully.',
+                'type'      => 'success',
+                'message'   => 'The record has been removed successfully.',
             ));
 
             return true;
-        } else {
-
-            $errors = $this->getForm()->getErrors();
-
-            foreach ($errors as $error) {
-                $this->getFlashBag()->add('message', array(
-                    'type' => 'error',
-                    'message' => $error->getMessage(),
-                ));
-            }
-
-            return false;
         }
+
+        return false;
     }
 }
